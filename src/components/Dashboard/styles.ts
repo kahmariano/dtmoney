b5ebcfab9203ee82a050f306d2a0256a6a1df253
kahmariano <kmariano138@gmail.com>
2021-11-22 22:10:25 -0300

    first commit

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+// ctrl+ . com um clique na palavra q da erro exibe o erro e importa automatico
+export const Container = styled.main`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 1rem;
+`
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  useEffect( ( ) => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, []);
+
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Títulos</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td >Desenvolvimento de website</td>
+            <td className="deposito">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>22/11/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.000</td>
+            <td>Casa</td>
+            <td>20/11/2021</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </Container>
+
+  );
+}
