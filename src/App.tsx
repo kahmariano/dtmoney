9cecb1d9f0ad4c2d15f8d68396f5310a3fe86698
kahmariano <kmariano138@gmail.com>
2021-11-23 14:24:23 -0300

    configurando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { useState } from 'react'
+
+Modal.setAppElement('#root')
+//a documentaçao pede pra fazer por causa de acessibilidade
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-  <Header/>
-  <Dashboard/>
-  <GlobalStyle/>
-    </>
-  );
-}
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        //isOpen abre modal
+        onRequestClose={handleCloseNewTransactionModal}
+        //onRequestClose fecha modal
+      >
+        <h2>Cadastras Nova Transação</h2>
+      </Modal>
+
+      <GlobalStyle />
+    </>
+  )
+}
